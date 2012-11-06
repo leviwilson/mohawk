5a35d8d9096047b92e284d3cc1bb8a81c985b9ea
Levi Wilson <levi@leviwilson.com>
2012-11-06 15:42:13 -0500

    spec for the exist? method

--- spec/lib/fado_spec.rb
+++ spec/lib/fado_spec.rb
@@ -9,14 +9,19 @@ describe Fado do
   let(:screen) { TestScreen.new }
   let(:window) { double('RAutomation Window') }
 
-  before(:each) do
-    #RAutomation::Window.stub(:new).and_return(window)
+  it "uses the uia adapter by default" do
+    RAutomation::Window.should_receive(:new).with(:title => "Some Window Title", :adapter => :ms_uia).and_return(window)
+    TestScreen.new
   end
 
   context "using the UI Automation adapter" do
-    it "should force :ms_uia" do
-      RAutomation::Window.should_receive(:new).with(:title => "Some Window Title", :adapter => :ms_uia).and_return(window)
-      TestScreen.new
+    before(:each) do
+      RAutomation::Window.stub(:new).and_return(window)
+    end
+
+    it "knows if a window exists" do
+      window.should_receive(:exist?)
+      screen.exist?
     end
   end
 end
