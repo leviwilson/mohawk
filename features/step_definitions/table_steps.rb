6d18321497d3c2973e2a6e34cac96f9a26498fa5
Levi Wilson <levi@leviwilson.com>
2012-12-08 15:37:26 -0500

    no need for fanciness with the table

--- features/step_definitions/table_steps.rb
+++ features/step_definitions/table_steps.rb
@@ -1,13 +1,10 @@
-When /^observing the "(.*?)" table$/ do |name|
+When /^we are observing the people table$/ do
   on(MainScreen).data_entry_form_button
-  on(DataEntryForm) do |screen|
-    @table_name = name.to_field
-    @table_rows = screen.send "#{name.to_field}_rows"
-  end
+  on(DataEntryForm).should be_active
 end
 
 Then /^the table row information should look like the following:$/ do |row_items|
   row_items.map_column!("row") { |r| r.to_i }
   row_items.map_headers!("text" => :text, "row" => :row)
-  @table_rows.should eq row_items.hashes
+  on(DataEntryForm).people_rows.should eq row_items.hashes
 end                                                                                  
--- features/table.feature
+++ features/table.feature
@@ -1,7 +1,9 @@
 Feature:  Working with tables
 
+Background:
+  Given we are observing the people table
+
 Scenario: Retrieving the row values
-  When observing the "people" table
   Then the table row information should look like the following:
     | text      | row |
     | John Doe  | 0   | 
