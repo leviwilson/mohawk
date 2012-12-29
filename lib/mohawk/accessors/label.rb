ce7ec58e4778c7d164fac4c6f62493e1c943b57d
Levi Wilson <levi@leviwilson.com>
2012-12-29 03:09:37 -0500

    spaces > tabs

--- lib/mohawk/accessors/label.rb
+++ lib/mohawk/accessors/label.rb
@@ -1,13 +1,13 @@
 module Mohawk
-	module Accessors
-		class Label
-			def initialize(adapter, locator)
-				@label = adapter.window.label(locator)
-			end
-			
-			def value 
-				@label.value
-			end
-		end
-	end
+  module Accessors
+    class Label
+      def initialize(adapter, locator)
+        @label = adapter.window.label(locator)
+      end
+
+      def value
+        @label.value
+      end
+    end
+  end
 end
