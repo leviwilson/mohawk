a928a33e7ad1c86391926a584ee9d76281161b17
Levi Wilson <levi@leviwilson.com>
2012-12-29 04:04:06 -0500

    specs for link control

---
+++ spec/lib/mohawk/accessors/link_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+class LinkScreen
+  include Mohawk
+
+  window(:title => nil)
+  link(:link_control, :id => "linkID")
+end
+
+describe Mohawk::Accessors::Label do
+  let(:screen) { LinkScreen.new }
+  let(:window) { double("RAutomation Window") }
+  let(:link_control) { double("Link Control") }
+
+  before(:each) do
+    RAutomation::Window.stub(:new).and_return(window)
+    window.should_receive(:label).with(:id => "linkID").and_return(link_control)
+  end
+
+  it "can retrieve the link text" do
+    link_control.should_receive(:value).and_return("some string")
+    screen.link_control_text.should eq("some string")
+  end
+
+  it "can click on the link" do
+    link_control.should_receive(:click)
+    screen.click_link_control
+  end
+
+end
