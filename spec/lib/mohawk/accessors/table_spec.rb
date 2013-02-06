9f4d1a67ea87ef408f614145641bf9caae797e11
Levi Wilson <levi@leviwilson.com>
2013-02-06 10:26:10 -0500

    remove unnecessary setup in table_spec

--- spec/lib/mohawk/accessors/table_spec.rb
+++ spec/lib/mohawk/accessors/table_spec.rb
@@ -110,7 +110,6 @@ describe Mohawk::Accessors::Table do
       it "clearly lets you know if a header is not there" do
         RAutomation::Adapter::MsUia::UiaDll.should_receive(:table_headers).and_return(["First Header", "Second Header"])
         table.should_receive(:hwnd)
-        expected_cells = [FakeTableRow.new("Item 1", 0), FakeTableRow.new("Item 2", 1)]
         lambda { screen.top[0].does_not_exist }.should raise_error ArgumentError, "does_not_exist column does not exist in [:first_header, :second_header]"
       end
     end
