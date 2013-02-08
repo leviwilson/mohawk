4352c593e15c7f84bf1bc3084fe05acf31183a24
Levi Wilson <levi@leviwilson.com>
2013-02-08 17:21:40 -0500

    feature for passing extra info to a screen

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -5,3 +5,11 @@ end
 Then /^we should still arrive at our destination$/ do
   on(DataEntryForm).should be_active
 end
+
+When /^we give extra navigational information to a screen$/ do
+  @which_screen = on(MainScreen, :pid => @process.pid)
+end
+                                                                   
+Then /^it should listen to every word that we say$/ do
+  @which_screen.should be_present
+end
