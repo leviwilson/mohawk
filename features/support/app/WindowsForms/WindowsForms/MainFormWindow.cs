2c7844b64c30f70f2ff58c7c5da4dda03f5e9652
Levi Wilson <levi@leviwilson.com>
2013-03-18 01:26:16 -0400

    add WindowsForms solution to mohawk

---
+++ features/support/app/WindowsForms/WindowsForms/MainFormWindow.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class MainFormWindow : Form
+    {
+        public MainFormWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutButton_Click(object sender, EventArgs e)
+        {
+            AboutBox aboutBox = new AboutBox();
+            aboutBox.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox.Checked == true)
+                checkBoxLabel.Text = "checkBox is on";
+            else
+                checkBoxLabel.Text = "checkBox is off";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButtonLabel.Text = "Option 1 selected";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButtonLabel.Text = "Option 2 selected";
+        }
+
+        private void radioButtonReset_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButtonLabel.Text = "No option selected";
+        }
+
+        private void FruitsComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fruitsLabel.Text = FruitsComboBox.Text;
+        }
+
+        private void nextFormButton_Click(object sender, EventArgs e)
+        {
+            DataEntryForm form = new DataEntryForm();
+            form.Show();
+        }
+
+        private void buttonButton_Click(object sender, EventArgs e)
+        {
+            SimpleElementsForm buttonForm = new SimpleElementsForm();
+            buttonForm.Show();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void automatableMonthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private int LinkClicks { get; set; }
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.Text = String.Format("{0} (clicked {1} times)", linkLabel1.Name, ++LinkClicks);
+        }
+    }
+}
