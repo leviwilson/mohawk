e9a93f02b23764ef95e2a5c1de58888dac8b51a3
Levi Wilson <levi@leviwilson.com>
2013-05-14 23:25:08 -0400

    stub FFI calls for all specs

---
+++ spec/ffi_stub.rb
@@ -0,0 +1,15 @@
+require 'ffi'
+
+module FFI
+  module Library
+    def ffi_lib(*names)
+    end
+
+    def ffi_libraries
+    end
+
+    def attach_function(name, func, args, returns = nil, options = nil)
+    end
+  end
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rspec'
+require 'ffi_stub'
 require 'mohawk'
 require 'coveralls'
 
