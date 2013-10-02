a54a7977c5e23425f395a48a526aba70316aea46
Levi Wilson <levi@leviwilson.com>
2013-10-02 13:06:30 -0400

    require rspec/mocks in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rspec'
+require 'rspec/mocks'
 require 'ffi_stub'
 require 'mohawk'
 require 'coveralls'
