a5758c5e61c5c8b2bb54f2b58da4a64838403401
Levi Wilson <levi@leviwilson.com>
2013-11-03 00:58:56 -0400

    added specs for Keys#encode for uia adapter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rspec'
 require 'rspec/mocks'
+require 'rspec-given'
 require 'ffi_stub'
 require 'mohawk'
 require 'coveralls'
