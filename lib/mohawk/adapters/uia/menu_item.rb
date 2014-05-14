94b865d2adbb2b247524a4baad52fdfa7d42fb9a
Levi Wilson <levi@leviwilson.com>
2014-05-14 00:09:14 -0400

    update MenuItem to use new uia for selecting menu items

--- lib/mohawk/adapters/uia/menu_item.rb
+++ lib/mohawk/adapters/uia/menu_item.rb
@@ -5,42 +5,25 @@ module Mohawk
         class MenuItemNotFound < StandardError; end
 
         def select
-          menu_item.as(:invoke).invoke
+          element.select_menu_item(*path)
         end
 
         def click
-          menu_item.click_center
+          element.menu_item(*path).click_center
         end
 
         def exist?
-          !!menu_item
-        rescue MenuItemNotFound
-          false
+          !!element.menu_item(*path)
         end
         alias_method :exists?, :exist?
 
         private
-        def menu_item
-          current_item = @parent
-          path.each_with_index do |menu_item, index|
-            current_item = current_item.filter(control_type: :menu_item, name: menu_item).first
-            raise MenuItemNotFound, menu_item unless current_item
-            try_to_expand current_item unless index == path.count - 1
-          end
-          current_item
+        def element
+          @parent.with(:menu_items)
         end
 
         def path
           [@locator[:path] || @locator[:text]].flatten
-        end
-
-        def try_to_expand(item)
-          case
-            when item.patterns.include?(:expand_collapse)
-              item.as(:expand_collapse).expand
-            else
-              item.as(:invoke).invoke
-          end
         end
       end
     end
