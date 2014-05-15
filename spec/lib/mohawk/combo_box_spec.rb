4bfaffc60dbb80465710d27a2bb127c5f2813c1d
Levi Wilson <levi@leviwilson.com>
2014-05-15 17:34:16 -0400

    move combo_box feature :arrow_right: combo_box_spec

---
+++ spec/lib/mohawk/combo_box_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe 'combo boxes' do
+  Given(:main_form) { start_app }
+
+  context '#combo_box= / #combo_box' do
+    context 'by index' do
+      When { main_form.fruits = 2 }
+      Then { main_form.fruits == 'Coconut' }
+    end
+
+    context 'by value' do
+      When { main_form.fruits = 'Coconut' }
+      Then { main_form.fruits == 'Coconut' }
+    end
+
+    context 'by Regex' do
+      When { main_form.fruits = /[Or]an/ }
+      Then { main_form.fruits == 'Orange' }
+    end
+
+    context 'multi-select' do
+      Given { main_form.toggle_multi }
+      When { (0..2).each {|n| main_form.select_fruits_list n } }
+
+      Then { main_form.fruits_list_selections == ['Apple', 'Orange', 'Mango'] }
+
+      context '#clear_' do
+        context 'by index' do
+          When { main_form.clear_fruits_list 2 }
+          Then { main_form.fruits_list_selections == ['Apple', 'Orange'] }
+        end
+
+        context 'by value' do
+          When { main_form.clear_fruits_list 'Orange' }
+          Then { main_form.fruits_list_selections == ['Apple', 'Mango'] }
+        end
+
+        context 'by Regex' do
+          When { main_form.clear_fruits_list /p{2}/ }
+          Then { main_form.fruits_list_selections == ['Orange', 'Mango'] }
+        end
+      end
+    end
+  end
+
+  context 'list boxes' do
+    # ListBox controls are special with regard to raising events
+    When { main_form.fruits_list = 'Orange' }
+    Then { main_form.fruits_label == 'Orange' }
+  end
+end
--- spec/screens/main_form.rb
+++ spec/screens/main_form.rb
@@ -8,12 +8,18 @@ class MainForm
 
   button(:about, id: 'aboutButton')
   button(:data_entry_form, value: 'Data Entry Form')
+  button(:toggle_multi, value: 'Toggle Multi-Select')
 
   checkbox(:checkbox, id: 'checkBox')
+
+  combo_box(:fruits, id: 'FruitsComboBox')
+  select_list(:fruits_list, id: 'FruitListBox')
 
   tree_view(:tree_view, id: 'treeView')
 
   label(:sample_label, id: 'label1')
+  label(:fruits_label, :id => 'fruitsLabel')
+
   link(:link_label, id: 'linkLabel1')
 
   control(:month_calendar, id: 'automatableMonthCalendar1')
