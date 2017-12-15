ec36a5fde16085378d9e77e264e06da5f56bb05d
Sam Glockner <samgl@SAMGLOCKNER-VM.teamnorthwoods.com>
2017-12-15 16:31:20 -0500

    Better error reporting when intializing screens
    ```
    class NonExistentScreen
      include Mohawk
      window(:title => /I don't exist/)
    end
    
    on(NonExistentScreen)
    ```
    
    Old: `Mohawk::Waiter::WaitTimeout: Mohawk::Waiter::WaitTimeout`
    New: `Mohawk::Waiter::WaitTimeout: Unable to locate 'NonExistentScreen'`

--- lib/mohawk/navigation.rb
+++ lib/mohawk/navigation.rb
@@ -4,11 +4,13 @@ module Mohawk
   module Navigation
     include PageNavigation
 
-    def on(cls, extra={}, &block)
+    def on(cls, extra = {}, &block)
       screen = cls.new extra
       screen.wait_until_present
       block.call screen if block
       screen
+    rescue Mohawk::Waiter::WaitTimeout => e
+      raise e.class, "Unable to locate '#{cls}'", e.backtrace
     end
   end
 end
--- spec/lib/mohawk/navigation_spec.rb
+++ spec/lib/mohawk/navigation_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
+class NonExistentScreen
+  include Mohawk
+  window(:title => /I don't exist/)
+end
+
 describe Mohawk::Navigation do
   context '#on with extra info' do
     When { start_app }
@@ -21,5 +26,13 @@ describe Mohawk::Navigation do
 
     When(:data_entry_form) { navigate_to(DataEntryForm) }
     Then { expect(data_entry_form).to be_present }
+  end
+
+  context 'name the screen that could not be found' do
+    When { Mohawk.timeout = 1 }
+    Then do
+      screen_class = NonExistentScreen
+      expect { on(screen_class) }.to raise_error Mohawk::Waiter::WaitTimeout, "Unable to locate '#{screen_class}'"
+    end
   end
 end
