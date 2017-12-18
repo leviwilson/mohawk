75ba21497bb46a77f5e4dd73b188bbf2027f52f3
Sam Glockner <samgl@SAMGLOCKNER-VM.teamnorthwoods.com>
2017-12-18 13:34:19 -0500

    Formatting

--- lib/mohawk/navigation.rb
+++ lib/mohawk/navigation.rb
@@ -4,7 +4,7 @@ module Mohawk
   module Navigation
     include PageNavigation
 
-    def on(cls, extra = {}, &block)
+    def on(cls, extra={}, &block)
       screen = cls.new extra
       screen.wait_until_present "Unable to locate #{cls} using #{screen.adapter.locator}"
       block.call screen if block
